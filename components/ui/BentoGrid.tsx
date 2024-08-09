193992ba673a7d6c7994e04cce8dff61ed2be591
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2024-08-09 13:50:11 +0300

    Spread Lottie props

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -164,7 +164,7 @@ return (
                 className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
                 }`}
             >
-                <Lottie options={defaultOptions} height={200} width={400} />
+                <Lottie options={...defaultOptions} height={200} width={400} />
             </div>
 
             <MagicButton
